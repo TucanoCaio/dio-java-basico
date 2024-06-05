bb006b866c8bcec22e547e4954b7021d6cb102cc
tucano <caio.tucano@outlook.com>
2024-06-05 20:11:22 -0300

    Docs update

--- CadernoEstudo.md
+++ CadernoEstudo.md
@@ -2256,3 +2256,31 @@ Caderno de anotações para estudo da trilha JAVA do Bootcamp ofereciso pelo San
         - `removerItem(String nome)`: Remove um item do carrinho com base no seu nome.
         - `calcularValorTotal()`: Calcula e rotorna o valor total do carrinho, levando em consideração o preço e a quantidade de cada item.
         - `exibirItens()`: Exibe todos os itens presentes no carrinho, mostrando seus nomes, preço e quantidades.
+
+    - Pesquisa List
+
+        #### 1. Catálogo de Livros
+
+        Crie um classe chamada "CatalogoLivros" que possui uma lista de objetos do tipo "Livro" como atributo. Cada livro possui atributos como título, autor e ano de publicação. Implemente os seguintes métodos:
+
+        - `adicionarLivro(String titulo, String autor, int anoPublicacao)`: Adiciona um livro ao catálogo.
+
+        - `pesquisarPorAutor(String autor)`: Pesquisa livros por autor e retorna um lista com os livros encontrados.
+
+        - `pesquisarPorIntervaloAnos(int anoInicial, int anoFinal)`: Pesquisa livros publicados em um determinado intervalo de anos e retorna uma lista com os livros encontrados.
+
+        - `pesquisarPorTitulo(String titulo)`: Pesquisa livros por título e retorna o primeiro livro encontrado.
+
+        #### 2. Soma de Números.
+
+        Crie uma classe chamada "SomaNumeros" que possui uma lista de números inteiros como atributo. Implemente os seguintes métodos:
+
+        - `adicinarNumero(int numero)`: Adiciona um número à lista de números.
+
+        - `calcularSoma()`: Calcula a soma de todos os números na lista e retorna o resultado.
+
+        - `encontrarMaiorNumero()`: Encontra o maior número na lista e retorna o valor.
+
+        - `encontrarMenorNumero()`: Encontra o menor número na listae retorna o valor.
+
+        - `exibirNumeros()`: Retorna uma lista contendo todos os numeros presentes na lista.
---
+++ java-collections-api/src/edu/collections/pesquisa/CatalogoLivros.java
@@ -0,0 +1,76 @@
+package edu.collections.pesquisa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros {
+    
+    private List<Livro> livroList;
+
+    public CatalogoLivros() {
+        this.livroList = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao){
+        livroList.add(new Livro(titulo, autor, anoPublicacao));
+    }
+
+    public List<Livro> pesquisarPorAutor(String autor){
+        List<Livro> livrosPorAutor = new ArrayList<>();
+
+        if (!livroList.isEmpty()) {
+            for (Livro l : livroList){
+                if (l.getAutor().equalsIgnoreCase(autor)) {
+                    livrosPorAutor.add(l);
+                }
+            }
+        }
+        return livrosPorAutor;
+    }
+
+    public List<Livro> pesquisarPorIntervaloAnos(int anoInicial, int anoFinal){
+        List<Livro> livrosPorAno = new ArrayList<>();
+
+        if (!livroList.isEmpty()) {
+            for(Livro l : livroList){
+                if (l.getAnoPublicacao()>=anoInicial&&l.getAnoPublicacao()<=anoFinal){
+                    livrosPorAno.add(l);
+                }
+            }
+        }
+        return livrosPorAno;
+    }
+
+    public Livro pesquisarPorTitulo(String titulo){
+        Livro livrosPorTitulo = null;
+
+        if (!livroList.isEmpty()) {
+            for (Livro l : livroList){
+                if (l.getTitulo().equalsIgnoreCase(titulo)) {
+                    livrosPorTitulo = l;
+                }
+                break;
+            }
+        }
+        return livrosPorTitulo;
+    }
+
+    public static void main(String[] args) {
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 1", 1999);
+        catalogoLivros.adicionarLivro("Livro 2", "Autor 1", 2000);
+        catalogoLivros.adicionarLivro("Livro 3", "Autor 1", 2002);
+        catalogoLivros.adicionarLivro("Livro 4", "Autor 2", 1999);
+        catalogoLivros.adicionarLivro("Livro 5", "Autor 2", 2002);
+        catalogoLivros.adicionarLivro("Livro 6", "Autor 3", 2019);
+        catalogoLivros.adicionarLivro("Livro 7", "Autor 4", 2022);
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 5", 2002);
+
+        //System.out.println(catalogoLivros.pesquisarPorAutor("Autor 2"));
+        //System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(2000, 2019));
+        //System.out.println(catalogoLivros.pesquisarPorTitulo("Livro 1"));
+    }
+    
+}
+
